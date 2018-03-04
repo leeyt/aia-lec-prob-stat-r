d62f9bee8a09dd98fa74162db54d04586d1becf0
Neil Lee <neil.lee@udngroup.com.tw>
2018-03-04 19:49:12 +0800

    Add data files

--- AI-D2-1.R
+++ AI-D2-1.R
@@ -1,5 +1,4 @@
 # 5/15
-
 # install.packages("TTR")
 library(TTR)
 data(ttrc)
@@ -17,48 +16,47 @@ lines(ema.20, col="blue", lwd=2)
 lines(wma.20, col="green", lwd=2)
 legend("topright", legend=c("sma.20", "ema.20", "wma.20"), col=c("red", "blue", "green"), lty=1, lwd=2)
 
-
 # 7/15
 data(cars)
 dim(cars)
 head(cars)
 
 par(mfrow=c(1, 3))
-for(i in c(0.1, 0.3, 0.5)){
+for(i in c(0.1, 0.3, 0.5)) {
   plot(cars$dist ~ cars$speed, main=paste0("lowess (f=", i,")"))
   lines(lowess(cars$dist ~ cars$speed, f = i), col="red", lwd=2)
 }
 
-
 # 11/15
-plot(density(iris$Sepal.Length))
+par(mfrow=c(2, 2))
+#plot(density(iris$Sepal.Length))
 hist(iris$Sepal.Length, prob=T)
 lines(density(iris$Sepal.Length), col="red")
 
+#plot(density(iris$Sepal.Width))
+hist(iris$Sepal.Width, prob=T)
+lines(density(iris$Sepal.Width), col="red")
+
+#plot(density(iris$Petal.Length))
+hist(iris$Petal.Length, prob=T)
+lines(density(iris$Petal.Length), col="red")
+
+#plot(density(iris$Petal.Width))
+hist(iris$Petal.Width, prob=T)
+lines(density(iris$Petal.Width), col="red")
+
 #13/15
-#install.packages("jpeg")
+# install.packages("jpeg")
 library(jpeg)
-ruddyduck.img <- readJPEG("ruddyduck.jpg")
+ruddyduck.img <- readJPEG("data/ruddyduck.jpg")
 plot(0, xlim=c(0, 14), ylim=c(-6, 4), type='n', xlab="", ylab="", main="Spline approximate to the top profile of the ruddy duck")
 rasterImage(ruddyduck.img, 0.6, -6, 13.8, 3.3)
 abline(v=1:14, h=-6:4, col="grey")
  
 # 14/15
-ruddyduck.dat <- read.table("ruddyduck.txt", header=T, sep="\t")
+ruddyduck.dat <- read.table("data/ruddyduck.txt", header=T, sep="\t")
 head(ruddyduck.dat)
 points(ruddyduck.dat, col="blue", pch=16)
 
 duck.spl <- smooth.spline(ruddyduck.dat$fx ~ ruddyduck.dat$x)
 lines(duck.spl, col = "red", lwd=2)
-
-
-
-
-
-
-
-
-
-
-
-
